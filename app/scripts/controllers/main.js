64962011677704c6b9f8949a6e45e4b325290f18
Emmanuel Mascarinas <eemascarinas@infiniteenergy.com>
2015-04-07 17:29:45 -0400

    asdf

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the epicoverflowApp
  */
 angular.module('epicoverflowApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+        .controller('MainCtrl', function ($scope) {
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+            $scope.auth = function () {
+                var clientId = '4593';
+                var scope = 'read_inbox,write_access,private_info';
+                var redirectUri = 'http://dunggoanan.com/emem/';
+                var url = 'https://stackexchange.com/oauth?scope=' + scope + '&client_id=' + clientId + '&redirect_uri=' + redirectUri;
+                window.location.replace(url);
+                
+            };
+        });
