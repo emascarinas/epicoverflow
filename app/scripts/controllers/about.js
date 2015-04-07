81772d06a49e7d89cc625cedf75a8357fcaaa6bf
Emmanuel Mascarinas <eemascarinas@infiniteenergy.com>
2015-04-07 14:35:25 -0400

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name epicoverflowApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the epicoverflowApp
+ */
+angular.module('epicoverflowApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name epicoverflowApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the epicoverflowApp
+ */
+angular.module('epicoverflowApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
