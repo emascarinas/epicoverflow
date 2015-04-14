f05c49bdacd0baea857643b60dd6b133af67da3b
Emmanuel Mascarinas <eemascarinas@infiniteenergy.com>
2015-04-14 15:32:53 -0400

    istambul

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,37 +28,43 @@ module.exports = function(config) {
             'test/spec/**/*.js',
         ],
         // list of files / patterns to exclude
-        exclude: [],
+        exclude: [
+        ],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'app/scripts/**/*.js': ['coverage']
+        },
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage',
+            subdir: '.'
+        },		
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage'],
         // web server port
-        port: 8080,
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera
-        // - Safari (only Mac)
-        // - PhantomJS
-        // - IE (only Windows)
+        port: 9876,
         browsers: [
             'PhantomJS'
         ],
-        // Which plugins to enable
         plugins: [
             'karma-phantomjs-launcher',
-            'karma-jasmine'
+            'karma-jasmine',
+            'karma-coverage'
         ],
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false,
+        // enable / disable colors in the output (reporters and logs)
         colors: true,
         // level of logging
-        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
-        // Uncomment the following lines if you are using grunt's server to run the tests
-        // proxies: {
-        //   '/': 'http://localhost:9000/'
-        // },
-        // URL root prevent conflicts with the site root
-        // urlRoot: '_karma_'
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
     });
 };
