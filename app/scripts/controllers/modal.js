60fc5f5fe4b780e5044f4b6e4ea9a1b6f1052785
Emmanuel Mascarinas <eemascarinas@infiniteenergy.com>
2015-04-17 09:31:38 -0400

    redirect of token expired

--- app/scripts/controllers/modal.js
+++ app/scripts/controllers/modal.js
@@ -3,6 +3,9 @@
 angular.module('epicoverflowApp').controller('ModalInstanceCtrl', function($scope, $modalInstance, error) {
     $scope.error = error;
     $scope.ok = function() {
+        if('access_token_required' === error.error_name){ //jshint ignore:line
+            window.location = '/emem/';
+        }
         $modalInstance.close();
     };
 });
